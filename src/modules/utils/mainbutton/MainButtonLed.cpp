879b42cf541b9a29c59ae25f932ce3b266100bbb
Konstantin Tcepliaev <f355@f355.org>
2026-08-01 15:47:23 +0200

    Add Z1 LED ring timing

--- src/modules/utils/mainbutton/MainButtonLed.cpp
+++ src/modules/utils/mainbutton/MainButtonLed.cpp
@@ -2,6 +2,76 @@
 
 #include "LPC17xx.h"
 
+#if defined(MACHINE_Z1)
+
+namespace {
+
+#define LED_NOP1 __NOP();
+#define LED_NOP2 LED_NOP1 LED_NOP1
+#define LED_NOP4 LED_NOP2 LED_NOP2
+#define LED_NOP16 LED_NOP4 LED_NOP4 LED_NOP4 LED_NOP4
+#define LED_NOP32 LED_NOP16 LED_NOP16
+#define LED_NOP64 LED_NOP32 LED_NOP32
+#define LED_HIGH_ONE LED_NOP64 LED_NOP16
+#define LED_HIGH_ZERO LED_NOP32
+#define LED_LOW LED_NOP64 LED_NOP4 LED_NOP2
+
+constexpr uint32_t LED_PIN_MASK = 1UL << 22;
+
+__attribute__((always_inline)) inline void write_z1_bit(bool one)
+{
+    const uint32_t interrupt_mask = __get_PRIMASK();
+    __disable_irq();
+    LPC_GPIO0->FIOSET = LED_PIN_MASK;
+    if(one) {
+        LED_HIGH_ONE
+    } else {
+        LED_HIGH_ZERO
+    }
+    LPC_GPIO0->FIOCLR = LED_PIN_MASK;
+    if(interrupt_mask == 0U) __enable_irq();
+    LED_LOW
+}
+
+}
+
+void mainbutton_led_write_strip(unsigned char R1, unsigned char G1, unsigned char B1,
+                                unsigned char R2, unsigned char G2, unsigned char B2,
+                                unsigned char R3, unsigned char G3, unsigned char B3,
+                                unsigned char R4, unsigned char G4, unsigned char B4,
+                                unsigned char R5, unsigned char G5, unsigned char B5)
+{
+    // Keep the step/unstep timers available, but defer PendSV, SlowTicker,
+    // communications, and other potentially long handlers until the frame is complete.
+    constexpr uint32_t frame_priority_mask = 3U << (8U - __NVIC_PRIO_BITS);
+    const uint32_t previous_priority_mask = __get_BASEPRI();
+    if(previous_priority_mask == 0U || previous_priority_mask > frame_priority_mask) {
+        __set_BASEPRI(frame_priority_mask);
+    }
+
+    const unsigned char groups[5][3] = {
+        {R1, G1, B1}, {R2, G2, B2}, {R3, G3, B3},
+        {R4, G4, B4}, {R5, G5, B5}
+    };
+    const unsigned char group_order[6] = {0, 1, 2, 3, 4, 1};
+    const unsigned char wire_order[3] = {1, 0, 2};
+
+    for(unsigned char group : group_order) {
+        for(unsigned char led = 0; led < 3; ++led) {
+            for(unsigned char channel : wire_order) {
+                const unsigned char value = groups[group][channel];
+                for(unsigned char bit = 0; bit < 8; ++bit) {
+                    write_z1_bit((value & (0x80U >> bit)) != 0U);
+                }
+            }
+        }
+    }
+
+    __set_BASEPRI(previous_priority_mask);
+}
+
+#else
+
 void mainbutton_led_write_strip(unsigned char R1, unsigned char G1, unsigned char B1,
                                 unsigned char R2, unsigned char G2, unsigned char B2,
                                 unsigned char R3, unsigned char G3, unsigned char B3,
@@ -602,3 +672,5 @@ void mainbutton_led_write_strip(unsigned char R1, unsigned char G1, unsigned cha
 		}
 	}
 }
+
+#endif
