3768787c8b84eeb6e1c5a35698071a6104dd4a50
Konstantin Tcepliaev <f355@f355.org>
2026-08-01 14:17:25 +0200

    Add rotary-aware tool-change parking

---
+++ src/libs/RotaryMovePolicy.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace rotary_move {
+
+enum class XYOrder {
+    together,
+    x_then_y,
+    y_then_x,
+};
+
+constexpr bool enabled(bool configured, bool rotary_fitted)
+{
+    return configured && rotary_fitted;
+}
+
+constexpr XYOrder clearance_order(bool split)
+{
+    return split ? XYOrder::y_then_x : XYOrder::together;
+}
+
+constexpr XYOrder destination_order(bool split)
+{
+    return split ? XYOrder::x_then_y : XYOrder::together;
+}
+
+} // namespace rotary_move
