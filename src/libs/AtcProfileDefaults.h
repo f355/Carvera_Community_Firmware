4cf34c718727555622020739e780dbf89a16472e
Konstantin Tcepliaev <f355@f355.org>
2026-08-02 08:22:58 +0200

    Derive Z1 tool-change positions from calibration

--- src/libs/AtcProfileDefaults.h
+++ src/libs/AtcProfileDefaults.h
@@ -15,6 +15,14 @@ struct Defaults {
     float clearance_z;
 };
 
+struct ManualCoordinates {
+    float parking_x;
+    float parking_y;
+    float probe_x;
+    float probe_y;
+    float probe_z;
+};
+
 constexpr Defaults legacy_defaults(bool carvera)
 {
     return carvera
@@ -22,6 +30,20 @@ constexpr Defaults legacy_defaults(bool carvera)
                    -8.0F, 37.5F, 22.5F, -75.0F, -3.0F, -3.0F}
         : Defaults{-108.0F, 126.0F, 196.0F, 1,
                    30.0F, 82.5F, 23.0F, -5.0F, -21.0F, -5.0F};
+}
+
+constexpr ManualCoordinates z1_manual_coordinates(float anchor_x, float anchor_y,
+                                                   float toolrack_offset_x,
+                                                   float toolrack_offset_y,
+                                                   float toolrack_z)
+{
+    return ManualCoordinates{
+        anchor_x + toolrack_offset_x + 132.0F,
+        anchor_y + toolrack_offset_y,
+        anchor_x + 181.0F,
+        anchor_y + 181.0F,
+        toolrack_z,
+    };
 }
 
 } // namespace atc_profile
