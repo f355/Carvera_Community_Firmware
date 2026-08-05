5af94c857805b99281f1395c95704dfee6418303
Konstantin Tcepliaev <f355@f355.org>
2026-08-05 09:17:05 +0200

    Format newly added sources

--- src/libs/RotaryMovePolicy.h
+++ src/libs/RotaryMovePolicy.h
@@ -3,24 +3,15 @@
 namespace rotary_move {
 
 enum class XYOrder {
-    together,
-    x_then_y,
-    y_then_x,
+  together,
+  x_then_y,
+  y_then_x,
 };
 
-constexpr bool enabled(bool configured, bool rotary_fitted)
-{
-    return configured && rotary_fitted;
-}
+constexpr bool enabled(bool configured, bool rotary_fitted) { return configured && rotary_fitted; }
 
-constexpr XYOrder clearance_order(bool split)
-{
-    return split ? XYOrder::y_then_x : XYOrder::together;
-}
+constexpr XYOrder clearance_order(bool split) { return split ? XYOrder::y_then_x : XYOrder::together; }
 
-constexpr XYOrder destination_order(bool split)
-{
-    return split ? XYOrder::x_then_y : XYOrder::together;
-}
+constexpr XYOrder destination_order(bool split) { return split ? XYOrder::x_then_y : XYOrder::together; }
 
-} // namespace rotary_move
+}  // namespace rotary_move
